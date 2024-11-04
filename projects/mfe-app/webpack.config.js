e2641a3cdc4e5a485ca08b579b8f6c4fd244ff1b
Alessandra Dias de Souza <lupina.sp@gmail.com>
2024-11-03 21:44:47 -0300

    Configurando um módulo de microfrontend para ser exportado

---
+++ projects/mfe-app/webpack.config.js
@@ -0,0 +1,58 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const share = mf.share;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, '../../tsconfig.json'),
+  [/* mapped paths to share */]);
+
+module.exports = {
+  output: {
+    uniqueName: "mfeApp",
+    publicPath: "auto",
+    scriptType: "text/javascript"
+  },
+  optimization: {
+    runtimeChunk: false
+  },
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    }
+  },
+  experiments: {
+    outputModule: true
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+        // library: { type: "module" },
+
+        // For remotes (please adjust)
+        name: "mfeApp",
+        filename: "remoteEntry.js", //pode ter o domínio/remote
+        exposes: {
+            // './Component': './projects/mfe-app/src/app/app.component.ts', //não é indicado exportar o app componente
+            './ProfileModule': './projects/mfe-app/src/app/profile/profile.module.ts', //não é indicado exportar o app componente
+        },
+
+        // For hosts (please adjust)
+        // remotes: {
+        //     "hostApp": "http://localhost:4200/remoteEntry.js",
+
+        // },
+
+        shared: share({
+          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+
+          ...sharedMappings.getDescriptors()
+        })
+
+    }),
+    sharedMappings.getPlugin()
+  ],
+};
